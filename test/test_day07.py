08e05bb79ab6cbd7a6c9f8cf683dfb7c3ce0e399
redrem95 <alexander.vollmer.95@hotmail.de>
2020-12-07 07:58:20 +0100

    Implemented Day07

---
+++ test/test_day07.py
@@ -0,0 +1,24 @@
+from test.test_numpy import NUMPY_INSTALLED
+
+if NUMPY_INSTALLED:
+    from AoC.Day import StarTask
+    from test import Day07 as TestDay
+
+    _testDay = TestDay.TestDay()
+
+
+    def test_task01():
+        log, result = _testDay.run(task=StarTask.Task01)
+        assert TestDay.RESULT_1 == result
+
+
+    def test_task02():
+        log, result = _testDay.run(task=StarTask.Task02)
+        assert TestDay.RESULT_2_1 == result[0]
+        assert TestDay.RESULT_2 == result[1]
+
+
+    def test_none():
+        # noinspection PyTypeChecker
+        log, result = _testDay.run(task=None)
+        assert result is None
