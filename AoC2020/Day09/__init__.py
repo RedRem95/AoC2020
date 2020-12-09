fb859b218cc2ea491836d6d279d28e8bacda7073
redrem95 <alexander.vollmer.95@hotmail.de>
2020-12-09 07:17:11 +0100

    Implemented Day09

--- AoC2020/Day09/__init__.py
+++ AoC2020/Day09/__init__.py
@@ -1,45 +1,61 @@
-from typing import Tuple
+import itertools
+from typing import Tuple, List, Optional
 
 from AoC.Day import Day, StarTask
-from AoC2020.Day08 import today_instructions as day08_instructions
-from AoC2020.Day08.InstructionCode import InstructionMachine, CodeLine
+
+NUMPY_MODE = False
+if NUMPY_MODE:
+    import numpy as np
+    from numpy import sum, min, max
 
 
 class Day09(Day):
     def convert_input(self, raw_input: bytes, task: StarTask) -> object:
         ret = []
         for line in [x for x in str(raw_input, "utf-8").split("\n") if len(x) > 0]:
-            ret.append(CodeLine.parse(line))
+            ret.append(int(line))
+        if NUMPY_MODE:
+            return np.array(ret)
         return ret
 
     def run(self, task: StarTask) -> Tuple[str, object]:
         if task == StarTask.Task01:
             return self._run01(task=StarTask.Task01)
         if task == StarTask.Task02:
-            return self._run02(task=StarTask.Task02)
+            return self._run02(task=StarTask.Task01)
         return "", None
 
     def _run01(self, task):
-        log = []
-        result = 0
-        data = self.get_input(task=task)
-        instructions = day08_instructions()
-        instructions.update({})
-        machine = InstructionMachine(machine_code=data, registered_instructions=instructions.values())
-        machine.run()
-        for data_line in data:
-            result += 1
-
-        return "\n".join(str(x) for x in log), result
+        preamble_length = self.get_day_config().get("preamble_length", 25)
+        weakness_i, weakness = self._find_weakness(data=self.get_input(task=task), preamble_length=preamble_length)
+        log = ""
+        if weakness_i > 0 and weakness is not None:
+            log = f"{weakness} in line {weakness_i + 1} does not consist of a sum of two of the previous " \
+                  f"{preamble_length} lines"
+        return log, weakness
 
     def _run02(self, task):
-        log = []
-        result = 0
         data = self.get_input(task=task)
-        for data_line in data:
-            result += 1
+        preamble_length = self.get_day_config().get("preamble_length", 25)
+        weakness_i, weakness = self._find_weakness(data=data, preamble_length=preamble_length)
+        log = [f"Weakness is {weakness} in line {weakness_i + 1}"]
+        for j in range(weakness_i + 1):
+            for k in range(j + 1, weakness_i + 1):
+                if weakness == sum(data[j:k]):
+                    mi = min(data[j:k])
+                    ma = max(data[j:k])
+                    log.append(f"Found crack in serious of lines {j + 1} to {k} of length {k - j}")
+                    log.append(f"Crack is {mi} + {ma} => {mi + ma}")
+                    return "\n".join(str(x) for x in log), mi + ma
+        return "", None
 
-        return "\n".join(str(x) for x in log), result
+    @staticmethod
+    def _find_weakness(data: List[int], preamble_length: int) -> Tuple[int, Optional[int]]:
+        for i in range(preamble_length, len(data)):
+            if not any(x + y == data[i] for x, y in itertools.product(data[i - preamble_length:i],
+                                                                      data[i - preamble_length:i])):
+                return i, data[i]
+        return -1, None
 
     def get__file__(self) -> str:
         return __file__
