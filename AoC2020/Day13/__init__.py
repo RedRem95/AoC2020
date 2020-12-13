619e59e78b2fcaede8a0b1c4846d7f163b3f4522
redrem95 <alexander.vollmer.95@hotmail.de>
2020-12-13 09:36:19 +0100

    Implemented Day13 Task 01

--- AoC2020/Day13/__init__.py
+++ AoC2020/Day13/__init__.py
@@ -1,4 +1,5 @@
-from typing import Tuple, List
+from math import ceil
+from typing import Tuple, List, Dict
 
 from AoC.Day import Day, StarTask
 
@@ -6,7 +7,17 @@ from AoC.Day import Day, StarTask
 class Day13(Day):
 
     def convert_input(self, raw_input: bytes, task: StarTask) -> object:
-        return [x for x in str(raw_input, "utf-8").split("\n") if len(x) > 0]
+        raw_data = [x for x in str(raw_input, "utf-8").split("\n") if len(x) > 0]
+
+        def _to_int(v: str):
+            try:
+                return int(v)
+            except ValueError:
+                pass
+            return None
+
+        return {"your_time": int(raw_data[0]),
+                "busses": [_to_int(y.strip()) for y in raw_data[1].split(",")]}
 
     def get__file__(self) -> str:
         return __file__
@@ -14,20 +25,30 @@ class Day13(Day):
     def run(self, task: StarTask) -> Tuple[str, object]:
         if task == StarTask.Task01:
             return self._run01(data=self.get_input(task=StarTask.Task01))
-        if task == StarTask.Task01:
+        if task == StarTask.Task02:
             return self._run02(data=self.get_input(task=StarTask.Task01))
         return "", None
 
-    def _run01(self, data: List) -> Tuple[str, object]:
+    def _run01(self, data: Dict) -> Tuple[str, object]:
         log = []
-        result = 0
-        for data_line in data:
-            result += data_line.__len__()
-        return "\n".join(str(x) for x in log), result
+        your_time = data["your_time"]
+        busses = sorted(x for x in data["busses"] if x is not None)
+        log.append(f"You arrive at {your_time}")
+        log.append(f"Busses in service: {', '.join(str(x) for x in busses)}")
+        best_bus, wait_time = sorted([(x, x - (your_time % x)) for x in busses], key=lambda x: x[1])[0]
+        log.append(f"Best bus you can take is {best_bus}. It leaves in {wait_time} at {your_time + wait_time}")
+        return "\n".join(str(x) for x in log), best_bus * wait_time
 
     def _run02(self, data: List) -> Tuple[str, object]:
+        return "", None
         log = []
-        result = 0
-        for data_line in data:
-            result += data_line.__len__()
-        return "\n".join(str(x) for x in log), result
+        min_search = self.get_day_config().get("min_search", 0)
+        busses = dict((x, y) for x, y in enumerate(data["busses"]) if y is not None)
+        log.append(f"Busses in service: {', '.join(str(x) for x in sorted(busses.values()))}")
+        current = ceil(min_search / busses[0]) * busses[0]
+        log.append(f"Earliest timestamp to search is {current}")
+        while True:
+            if all(((current + i) % x) == 0 for i, x in busses.items()):
+                log.append(f"Found timestamp {current} where the rule is true")
+                return "\n".join(str(x) for x in log), current
+            current += busses[0]
