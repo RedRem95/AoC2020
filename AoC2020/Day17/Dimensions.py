e3a6352dfaba217b5703c2915b3e57769c30f686
redrem95 <alexander.vollmer.95@hotmail.de>
2020-12-17 08:02:24 +0100

    Implemented Day06

---
+++ AoC2020/Day17/Dimensions.py
@@ -0,0 +1,156 @@
+import itertools
+from abc import ABC, abstractmethod
+from enum import Enum
+from typing import Optional, List, Union, Tuple, Dict
+
+import numpy as np
+
+
+class State(Enum):
+    ACTIVE = "#",
+    INACTIVE = "."
+
+    @classmethod
+    def by_value(cls, val: str) -> Optional["State"]:
+        for i in cls:
+            if val == str(i):
+                return i
+        return None
+
+    def __str__(self):
+        return str(self.value[0] if hasattr(self.value, "__iter__") else self.value)
+
+
+class DimensionalRule(ABC):
+    @abstractmethod
+    def apply(self, me: State, adjacent: List[State]) -> Optional[State]:
+        pass
+
+
+class PocketDimension:
+
+    def __init__(self,
+                 initial_plane: List[List[Union[str, State]]],
+                 rules: List[DimensionalRule],
+                 dimensions: int = 3,
+                 only_interesting: Optional[List[State]] = None):
+        self._data: Dict[Tuple[int, ...], Optional[State]] = {}
+        zero_dimensions = [0] * (dimensions - 2)
+        self._rules = [x for x in rules]
+        self._dimensions = dimensions
+        self._only_interesting = only_interesting
+        for y, line in enumerate(initial_plane):
+            for x, element in enumerate(line):
+                state = State.by_value(str(element))
+                if state is not None and self._is_state_interesting(state):
+                    self._data[tuple([x, y] + zero_dimensions)] = State.by_value(str(element))
+
+    def _is_state_interesting(self, state: State):
+        if self._only_interesting is None:
+            return True
+        return state in self._only_interesting
+
+    def _get_adjacent(self, *coordinates: int) -> List[Tuple[int, ...]]:
+        ret: List[Tuple[int, ...]] = []
+        for delta in itertools.product(range(-1, 2, 1), repeat=len(coordinates)):
+            if all(d == 0 for d in delta):
+                continue
+            ret.append(tuple(c + d for c, d in zip(coordinates, delta)))
+        return ret
+
+    def get_at(self, *coordinates: int) -> State:
+        return self._data.get(coordinates, State.INACTIVE)
+
+    def next(self):
+        next_stage: Dict[Tuple[int, ...], Optional[State]] = {}
+
+        work_coordinates = set()
+        work_coordinates.add(tuple([0] * self._dimensions))
+        for c in self._data.keys():
+            if len(c) != self._dimensions:
+                raise ArithmeticError()
+            work_coordinates.add(c)
+            for a in self._get_adjacent(*c):
+                if len(a) != self._dimensions:
+                    raise ArithmeticError()
+                work_coordinates.add(a)
+
+        for coordinate in work_coordinates:
+            adjacent_coordinates = self._get_adjacent(*coordinate)
+            me = self.get_at(*coordinate)
+            adjacent = [self.get_at(*c) for c in adjacent_coordinates]
+            r = None
+            for rule in self._rules:
+                r = rule.apply(me=me, adjacent=adjacent)
+                if r is not None:
+                    break
+            if self._is_state_interesting(r):
+                next_stage[coordinate] = r
+
+        something_changed = self.compare_stages(s1=self._data, s2=next_stage)
+        self._data = next_stage
+        return something_changed
+
+    @staticmethod
+    def compare_stages(s1: Dict[Tuple[int, ...], Optional[State]], s2: Dict[Tuple[int, ...], Optional[State]]):
+        for coordinate in set([x for x in s1.keys()] + [x for x in s2.keys()]):
+            if s1.get(coordinate, None) != s2.get(coordinate, None):
+                return False
+        return True
+
+    def count(self, target_state: State):
+        return sum(1 if v == target_state else 0 for v in self._data.values())
+
+    def __str__(self):
+        z_dimensions = self._dimensions - 2
+        if z_dimensions > 0:
+            if z_dimensions > 1:
+                z_template = "z_{i}={z}"
+            else:
+                z_template = "z={z}"
+        else:
+            z_template = None
+        all_coordinates = np.array([x for x in self._data.keys()])
+        min_coordinates = np.min(all_coordinates, axis=0)
+        max_coordinates = np.max(all_coordinates, axis=0)
+
+        ret = []
+
+        all_zs = itertools.product(*[range(mi, ma + 1, 1) for mi, ma in zip(min_coordinates[2:], max_coordinates[2:])])
+
+        for zs in all_zs:
+            if len(ret) > 0:
+                ret.append("")
+            if z_template is not None:
+                ret.append(", ".join(z_template.format(i=i, z=z) for i, z in enumerate(zs)))
+            line = []
+            for y in range(min_coordinates[1], max_coordinates[1] + 1, 1):
+                for x in range(min_coordinates[0], max_coordinates[0] + 1, 1):
+                    line.append(str(self.get_at(*tuple((x, y) + zs))))
+                ret.append("".join(line))
+                line = []
+
+        return "\n".join(ret)
+
+    def get_dimensionality(self):
+        return self._dimensions
+
+
+class ItsAlive(DimensionalRule):
+    def apply(self, me: State, adjacent: List[State]) -> Optional[State]:
+        if me == State.ACTIVE:
+            if sum(1 if a == State.ACTIVE else 0 for a in adjacent) in (2, 3):
+                return State.ACTIVE
+            else:
+                return State.INACTIVE
+        return None
+
+
+class ItsDead(DimensionalRule):
+    def apply(self, me: State, adjacent: List[State]) -> Optional[State]:
+        if me == State.INACTIVE:
+            if sum(1 if a == State.ACTIVE else 0 for a in adjacent) == 3:
+                return State.ACTIVE
+            else:
+                return State.INACTIVE
+        return None
--- AoC2020/Day17/__init__.py
+++ AoC2020/Day17/__init__.py
@@ -1,33 +1,27 @@
 from typing import Tuple, List
 
 from AoC.Day import Day, StarTask
+from AoC2020.Day17.Dimensions import PocketDimension, ItsAlive, ItsDead, State
 
 
 class Day17(Day):
     def convert_input(self, raw_input: bytes, task: StarTask) -> object:
-        return [x.strip() for x in str(raw_input, "utf-8").strip().split("\n") if len(x) > 0]
+        return [[y for y in x.strip()] for x in str(raw_input, "utf-8").strip().split("\n") if len(x) > 0]
 
     def run(self, task: StarTask) -> Tuple[str, object]:
-        if task == StarTask.Task01:
-            return self._run01(data=self.get_input(task=task))
-        if task == StarTask.Task02:
-            return self._run01(data=self.get_input(task=task))
+        if task is not None:
+            return self._run(data=self.get_input(task=task), **self.get_day_config()[task.name])
         return "", None
 
-    def _run01(self, data: List[str]) -> Tuple[str, object]:
+    def _run(self, data: List[List[str]], steps: int = 6, dimensions: int = 3) -> Tuple[str, object]:
+        dimension = PocketDimension(initial_plane=data,
+                                    rules=[ItsAlive(), ItsDead()],
+                                    dimensions=dimensions,
+                                    only_interesting=[State.ACTIVE])
         log = []
-        result = 0
-
-        for line in data:
-            result *= len(line)
-
-        return "\n".join(str(x) for x in log), result
-
-    def _run02(self, data: List[str]) -> Tuple[str, object]:
-        log = []
-        result = 0
-
-        for line in data:
-            result *= len(line)
-
-        return "\n".join(str(x) for x in log), result
+        for i in range(steps):
+            dimension.next()
+        log.append(f"State after {steps} simulated steps")
+        r = dimension.count(State.ACTIVE)
+        log.append(f"After {steps} steps this {dimension.get_dimensionality()}-d pocket dimension has {r} active cores")
+        return "\n".join(str(x) for x in log), dimension.count(State.ACTIVE)
