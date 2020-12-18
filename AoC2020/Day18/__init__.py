2bd4ad526007475b7a773ad30672a1c284fe5c5b
redrem95 <alexander.vollmer.95@hotmail.de>
2020-12-18 07:51:14 +0100

    Implemented Day18

--- AoC2020/Day18/__init__.py
+++ AoC2020/Day18/__init__.py
@@ -1,6 +1,7 @@
 from typing import Tuple, List
 
 from AoC.Day import Day, StarTask
+from AoC2020.Day18.Math import Formula, Operators
 
 
 class Day18(Day):
@@ -9,15 +10,16 @@ class Day18(Day):
 
     def run(self, task: StarTask) -> Tuple[str, object]:
         if task is not None:
-            return self._run(data=self.get_input(task=task))
+            return self._run(data=self.get_input(task=task), order=self.get_day_config().get(task.name, None))
         return "", None
 
-    def _run(self, data: List) -> Tuple[str, object]:
-        log = []
-
-        r = None
-
-        for item in data:
-            pass
-
+    def _run(self, data: List[str], order: List[str]) -> Tuple[str, object]:
+        formulas = [Formula(x) for x in data]
+        log = [f"Going to calculate {len(formulas)} formulas"]
+        if order is not None:
+            log.append(f"Order of operators is: {', '.join(str(x) for x in order)}")
+        else:
+            log.append("No operator order given. Calculating from left to right only")
+        r = sum(f.calculate(order=order) for f in formulas)
+        log.append(f"Sum of all results is {r}")
         return "\n".join(str(x) for x in log), r
