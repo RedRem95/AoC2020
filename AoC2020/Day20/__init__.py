d90ffa8372efe6d198ebf3badecc4c2bcee590e7
redrem95 <alexander.vollmer.95@hotmail.de>
2020-12-19 08:35:26 +0100

    Prepared Day20

---
+++ AoC2020/Day20/__init__.py
@@ -0,0 +1,20 @@
+from typing import Tuple, List
+
+from AoC.Day import Day, StarTask
+
+
+class Day20(Day):
+
+    def convert_input(self, raw_input: bytes, task: StarTask) -> object:
+        return [x.strip() for x in str(raw_input, "utf-8").strip().split("\n") if len(x) > 0]
+
+    def run(self, task: StarTask) -> Tuple[str, object]:
+        if task is not None:
+            return self._run(data=self.get_input(task=task))
+        return "", None
+
+    def _run(self, data: List[str]) -> Tuple[str, object]:
+        log = []
+        r = None
+
+        return "\n".join(str(x) for x in log), r
