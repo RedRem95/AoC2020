0d72f039fcf214010ed34de6be1371f7083fa0a0
redrem95 <alexander.vollmer.95@hotmail.de>
2020-12-26 17:24:27 +0100

    Improved code for Day23

--- AoC2020/Day23/__init__.py
+++ AoC2020/Day23/__init__.py
@@ -1,8 +1,5 @@
 import collections
-from copy import deepcopy
-from typing import Tuple, List, Dict, Any, Optional
-
-import numpy as np
+from typing import Tuple, List, Dict, Optional
 
 from AoC.Day import Day, StarTask
 
@@ -19,144 +16,62 @@ class Day23(Day):
     def run(self, task: StarTask) -> Tuple[str, object]:
         if task is None:
             return "", None
-        log, cups = self._run_v2(data=self.get_input(task=task), **self.get_day_config()[task.name])
+        log, cups = self._run(cups=self.get_input(task=task), **self.get_day_config()[task.name])
         index_1 = cups.index(1)
         r: Optional[List[int]] = None
         if task == StarTask.Task01:
             r = [cups[(index_1 + i) % len(cups)] for i in range(1, len(cups), 1)]
-            log = []
+            log.append(f"Cups after '1' are labelled {' '.join(str(x) for x in r)}")
         elif task == StarTask.Task02:
-            r = [cups[(index_1 + 1) % len(cups)] * cups[(index_1 + 2) % len(cups)]]
-            log = []
+            wanted_cup_count = 2
+            r = [cups[(index_1 + i) % len(cups)] for i in range(1, wanted_cup_count + 1, 1)]
+            log.append(f"The {wanted_cup_count} cups after '1' are {' '.join(str(x) for x in r)}")
+            _r = r[0]
+            for _ in r[1:]:
+                _r *= _
+            r = [_r]
         return "\n".join(str(x) for x in log), None if r is None else "".join(str(x) for x in r)
 
     @staticmethod
-    def _run(data: List[int], rounds: int, target_amount: int, log: bool = False) -> Tuple[List[str], List[int]]:
-        log = [] if log else None
-        cups = data + [i + 1 for i in range(max(data), target_amount, 1)]
-        current_pos = 0
-        done_moves: List[List[int]] = [deepcopy(cups)]
-        for i in range(rounds):
-            move = perform_next(cups=cups, current_pos=current_pos)
-            if log:
-                log.append(f"--move {i + 1}--")
-                log.append(f"Cups: {' '.join(f'({i})' if i == move['current'] else str(i) for i in cups)}")
-                log.append(f"Pick up: {' '.join(str(i) for i in move['pick_up'])}")
-                log.append(f"Destination: {move['destination']}")
-                log.append("")
-            cups = move["next_cups"]
-            if cups in done_moves:
-                print("RECURSION!!!")
-                exit(0)
-            done_moves.append(deepcopy(cups))
-            current_pos = move["current_pos"] + 1
-            if rounds > 1000 and i % 1000 == 0:
-                print(f"{(i + 1) / rounds * 100:6.2f} [{i + 1}/{rounds}]")
-        if log:
-            log.append(f"--final--")
-            log.append(f"Cups: {' '.join(str(i) for i in cups)}")
-        return log, cups
+    def _run(cups: List[int], rounds: int, target_amount: int, pick_up_count: int = 3) -> Tuple[List[str], List[int]]:
+        log = [
+            f"Playing against crab with {target_amount if target_amount > 0 else len(cups)} cups",
+            f"First {len(cups)} cups are labeled {' '.join(str(x) for x in cups)}",
+            "Rest of cups are filled automatically",
+            f"Going to play {rounds} rounds",
+            f"Picking up {pick_up_count} cups per turn"
+        ]
+        cups = cups + [i + 1 for i in range(max(cups), target_amount, 1)]
+        min_cup, max_cup = min(cups), max(cups)
+        picked_up: Dict[int, List[int]] = {}
+        cups_queue = collections.deque(iterable=cups, maxlen=len(cups))
 
-    @staticmethod
-    def _run_v2(data: List[int], rounds: int, target_amount: int) -> Tuple[List[str], List[int]]:
-        log = []
-        cups = data + [i + 1 for i in range(max(data), target_amount, 1)]
-        cups = queue_variant(cups=cups, rounds=rounds)
-        return log, list(cups)
+        def get_next() -> int:
+            n = cups_queue.popleft()
+            if n in picked_up:
+                for v in reversed(picked_up[n]):
+                    cups_queue.appendleft(v)
+                del picked_up[n]
+            return n
 
+        i = 0
+        while i < rounds:
 
-def perform_next(cups: List[int], current_pos: int, pick_up_count: int = 3) -> Dict[str, Any]:
-    pick_up = [cups[(current_pos + i) % len(cups)] for i in range(1, pick_up_count + 1, 1)]
-    current = cups[current_pos % len(cups)]
-    destination = current - 1
-    while destination < min(cups) or destination in pick_up:
-        destination -= 1
-        if destination < min(cups):
-            destination = max(cups)
+            current = get_next()
+            pick_up = [get_next() for _ in range(pick_up_count)]
 
-    for i in pick_up:
-        cups.remove(i)
-    destination_index = cups.index(destination) + 1
-    next_cups = cups[:destination_index] + pick_up + cups[destination_index:]
-    # for i in reversed(pick_up):
-    #    next_cups.insert(destination_index, i)
-    return {
-        "cups": cups,
-        "pick_up": pick_up,
-        "current": current,
-        "destination": destination,
-        "next_cups": next_cups,
-        "current_pos": next_cups.index(current)
-    }
+            destination = current - 1
+            while destination < min_cup or destination in pick_up:
+                destination -= 1
+                if destination < min_cup:
+                    destination = max_cup
 
+            picked_up[destination] = pick_up
+            cups_queue.append(current)
 
-def numpy_variant(cups: List[int], rounds: int, pick_up_count: int = 3) -> np.ndarray:
-    _destination_search_old = 0
-    _destination_search_new = 0
-    _new_line = 0
-    l_c = len(cups)
-    matrix = np.zeros(shape=(2, l_c), dtype=np.int32)
-    matrix[0, :] = np.array(cups, dtype=np.int32)
-    cups = np.array(cups, dtype=np.int32)
-    now, prev = 0, 1
-    i = 1
-    while i <= rounds:
-        now, prev = i % 2, (i - 1) % 2
-        # matrix[now, :] = 0
+            i += 1
 
-        if i > 1 and (matrix[prev, :] == cups).all():
-            print(f"LOOP at {i}")
-            exit()
-
-        destination_candidates = matrix[prev, pick_up_count + 1:]
-        destination = np.argmin(destination_candidates)
-        if destination_candidates[destination] > matrix[prev, 0]:
-            destination = np.argmax(destination_candidates)
-
-        destination += 1
-
-        matrix[now, :destination] = matrix[prev, pick_up_count + 1:pick_up_count + destination + 1]
-        matrix[now, destination:destination + pick_up_count] = matrix[prev, 1:pick_up_count + 1]
-        matrix[now, destination + pick_up_count:-1] = matrix[prev, pick_up_count + destination + 1:]
-        matrix[now, -1] = matrix[prev, 0]
-
-        if rounds > 1000 and i % 1000 == 0:
-            print(f"{i / rounds * 100:6.2f} [{i}/{rounds}]")
-        i += 1
-    return matrix[now]
-
-
-def queue_variant(cups: List[int], rounds: int, pick_up_count: int = 3) -> List[int]:
-    min_cup, max_cup = min(cups), max(cups)
-    picked_up: Dict[int, List[int]] = {}
-    cups_queue = collections.deque(iterable=cups, maxlen=len(cups))
-
-    def get_next() -> int:
-        n = cups_queue.popleft()
-        if n in picked_up:
-            for v in reversed(picked_up[n]):
-                cups_queue.appendleft(v)
-            del picked_up[n]
-        return n
-
-    i = 0
-    while i < rounds:
-
-        current = get_next()
-        pick_up = [get_next() for _ in range(pick_up_count)]
-
-        destination = current - 1
-        while destination < min_cup or destination in pick_up:
-            destination -= 1
-            if destination < min_cup:
-                destination = max_cup
-
-        picked_up[destination] = pick_up
-        cups_queue.append(current)
-
-        i += 1
-
-    ret = []
-    while len(cups_queue) > 0:
-        ret.append(get_next())
-    return ret
+        ret = []
+        while len(cups_queue) > 0:
+            ret.append(get_next())
+        return log, ret
